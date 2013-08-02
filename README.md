15a6542a68ec1fc7357a2fef6ec21a8ac56b61cb
Mike <mike@evulse.com>
2013-08-02 11:32:49 +1000

    Switch to match finder selecting methods

--- README.md
+++ README.md
@@ -1,36 +1,37 @@
-# jQuery Bulk Select
+# jQuery finderSelect
 
-[jQuery Bulk Select](http://github.com/evulse/bulkselect) Adds the ability to highlight table rows based on the standard single click, command/ctrl+click and shift+click methods. This plugin will add a `selected` class to all active rows. It also is fully compatiable with Jquery UI draggable.
+[jQuery finderSelect](http://github.com/evulse/finderselect) Adds the ability to highlight table rows based on the standard single click, command/ctrl+click, command+drag/ctrl+drag and shift+click methods. This plugin will add a `selected` class to all active rows.
 
 ## How to Use
 
 To enable selecting on all tables simply call
 
-    $('table').bulk_select();
+    $('table').finderSelect();
 
 If you do not want to allow selecting of headers and footers use
 
-    $('table tbody').bulk_select();
+    $('table tbody').finderSelect();
 
 If you want to use a custom class for highlighting such as using Twitter Bootstrap
 
-    $('table tbody').bulk_select({'class':'warning'}); // Will Highlight Yellow in Twitter Bootstrap 3 RC1
+    $('table tbody').finderSelect({class:'warning'}); // Will Highlight Yellow in Twitter Bootstrap 3 RC1
 
-To allow Bulk Select to work on other children elements other than tr specify the children setting.
+To allow finderSelect to work on other children elements other than tr specify the children setting.
 
-    $('div').bulk_select({'class':'label-success','children':'span'});
+    $('div').finderSelect({class:'label-success',children:'span'});
 
 All available settings
 
     var options = {
-        'class': 'selected',
-        'children': 'tr',
-        'event': 'click',
-        'cursor': 'pointer'
+        class: 'selected',
+        children: 'tr',
+        event: 'click',
+        cursor: 'pointer'
     };
 
 ## Change Log
 
+*    0.1.4: Change to match Finder highlighting and rename from Batch Select to finderSelect.
 *    0.1.3: Fix Incorrect URL's in jQuery Plugin Manifest
 *    0.1.2: Rename Plugin
 *    0.1.1: Use the original parent rather than relying on .parent()
@@ -38,5 +39,5 @@ All available settings
 
 
 ## Live Demos
-*    [Basic Demo](http://evulse.github.io/bulkselect "jQuery Bulk Select - Demo")
+*    [Basic Demo](http://evulse.github.io/bulkselect "jQuery finderSelect - Demo")
 
---
+++ jquery.finderselect.js
@@ -0,0 +1,172 @@
+// jQuery finderSelect: a jQuery plugin that activates selecting elements
+// within a parent with Ctrl+Click, Command+Click and Shift+Click.
+//
+// Copyright 2013 Mike Angell
+//
+// Please see:
+//
+// https://github.com/evulse/finderselect
+//
+// For complete documentation.
+
+(function( $ ) {
+    $.fn.finderSelectHighlight = function(cssClass) {
+        if(cssClass === undefined) {
+            cssClass = 'selected';
+        }
+        return $(this).addClass(cssClass);
+    };
+
+    $.fn.finderSelectUnHighlight = function(cssClass) {
+        if(cssClass === undefined) {
+            cssClass = 'selected';
+        }
+        return $(this).removeClass(cssClass);
+    };
+
+    $.fn.finderSelectHighlighted = function(cssClass) {
+        if(cssClass === undefined) {
+            cssClass = 'selected';
+        }
+        return $(this).hasClass(cssClass);
+    };
+
+    $.fn.finderSelectToggleHighlight = function(cssClass) {
+        if(cssClass === undefined) {
+            cssClass = 'selected';
+        }
+        return $(this).toggleClass(cssClass);
+    };
+
+    $.fn.finderSelectBetween = function(a, b) {
+        if(a > b) {
+            return $(this).filter(':lt('+a+')').filter(':gt('+b+')');
+        } else {
+            return $(this).filter(':lt('+b+')').filter(':gt('+a+')');
+        }
+    };
+
+    $.fn.finderSelectDisableSelection = function() {
+        return $(this).css({'-moz-user-select':'-moz-none',
+                '-moz-user-select':'none',
+                '-o-user-select':'none',
+                '-khtml-user-select':'none', /* you could also put this in a class */
+                '-webkit-user-select':'none',/* and add the CSS class here instead */
+                '-ms-user-select':'none',
+                'user-select':'none'
+            }).bind('selectstart', function(){ return false; });
+    };
+
+    $.fn.finderSelect = function(user) {
+
+        var options = {
+            class: 'selected',
+            children: 'tr',
+            event: 'mousedown',
+            cursor: 'pointer'
+        };
+
+        $.extend(options, user);
+
+        $(this).finderSelectDisableSelection();
+
+        $(this).find(options.children).css('cursor', options.cursor);
+
+        var parent = $(this);
+        parent.data('down', false);
+
+        parent.mousedown(function() {
+            parent.data('down', true);
+        }).mouseup(function() {
+            parent.data('down', false);
+        });
+
+        $(this).on('mouseenter', options.children, function(e){
+            if (parent.data('down') && (e.ctrlKey || e.metaKey)) {
+                $(this).finderSelectToggleHighlight(options.class);
+            }
+        });
+
+        $(this).on(options.event, options.children, function(e){
+            var selected = $(this);
+            var last = parent.data('finderSelectPrimary');
+            var shift = parent.data('finderSelectSecondary');
+            var siblings = parent.find(options.children);
+            var clicked = siblings.index(this);
+            var next = siblings.index(siblings.eq(last).nextAll('.'+options.class).first());
+            console.log(last);
+            if(!siblings.eq(last).finderSelectHighlighted(options.class)) {
+                last = next;
+            }
+
+
+                if (e.shiftKey) {
+                    if(last == null) { last = 0;}
+                    if(shift != null) {
+                        selected.finderSelectHighlight(options.class);
+
+                        if(last < shift && last < clicked && clicked > shift) {
+                            siblings.finderSelectBetween(shift, clicked).finderSelectHighlight(options.class);
+                        }
+                        if(last < shift && last < clicked && clicked < shift) {
+                            siblings.eq(shift).finderSelectUnHighlight(options.class);
+                            siblings.finderSelectBetween(shift, clicked).finderSelectUnHighlight(options.class);
+                        }
+                        if(last < shift && last > clicked && clicked < shift) {
+                            siblings.eq(shift).finderSelectUnHighlight(options.class);
+                            siblings.finderSelectBetween(shift, last).finderSelectUnHighlight(options.class);
+                            siblings.finderSelectBetween(clicked, last).finderSelectHighlight(options.class);
+                        }
+                        if(last > shift && last < clicked && clicked > shift) {
+                            siblings.eq(shift).finderSelectUnHighlight(options.class);
+                            siblings.finderSelectBetween(shift, last).finderSelectUnHighlight(options.class);
+                            siblings.finderSelectBetween(clicked, last).finderSelectHighlight(options.class);
+                        }
+                        if(last > shift && last > clicked && clicked > shift) {
+                            siblings.eq(shift).finderSelectUnHighlight(options.class);
+                            siblings.finderSelectBetween(shift, clicked).finderSelectUnHighlight(options.class);
+                        }
+                        if(last > shift && last > clicked && clicked < shift) {
+                            siblings.finderSelectBetween(shift, clicked).finderSelectHighlight(options.class);
+                        }
+
+                        if(last == shift && shift > clicked) {
+                            siblings.finderSelectBetween(shift, clicked).finderSelectHighlight(options.class);
+                        }
+
+                        if(last == shift && shift < clicked) {
+                            siblings.finderSelectBetween(shift, clicked).finderSelectHighlight(options.class);
+                        }
+
+                    } else {
+
+                        siblings.finderSelectBetween(clicked, last).finderSelectHighlight(options.class);
+
+                        selected.finderSelectHighlight(options.class);
+                    }
+                    parent.data('finderSelectPrimary', last);
+                    parent.data('finderSelectSecondary', clicked);
+                } else {
+                    if (e.ctrlKey || e.metaKey) {
+                        if(selected.finderSelectHighlighted(options.class)) {
+                            selected.finderSelectUnHighlight(options.class);
+                            parent.data('finderSelectPrimary', clicked)
+                            parent.data('finderSelectSecondary', null);
+                        } else {
+                            selected.finderSelectHighlight(options.class);
+                            parent.data('finderSelectPrimary', clicked)
+                            parent.data('finderSelectSecondary', null);
+                        }
+
+                    } else {
+                        siblings.finderSelectUnHighlight(options.class);
+                        selected.finderSelectHighlight(options.class);
+                        parent.data('finderSelectPrimary', clicked)
+                        parent.data('finderSelectSecondary', null)
+                    }
+                }
+            console.log("Click:"+clicked+" Last:"+last+" Shift:"+shift+" Next:"+next);
+
+        });
+    };
+}( jQuery ));
